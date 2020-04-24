827fee1c87d19f4db34e15f202f9ea32f6366f63
Ryan Spishock <22054759+rspishock@users.noreply.github.com>
2020-04-24 15:56:45 -0400

    Added pickers for password type and length.

--- PassGen/Controller/ViewController.swift
+++ PassGen/Controller/ViewController.swift
@@ -8,25 +8,56 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
     
     var generatePassword = GeneratePassword()
+    let passwordTypes = ["Numeric", "Lower Case", "Upper Case", "Mixed Case",  "Alphanumeric", "Alphanumeric With Special Characters"]
+    let lenghtOfPassword = ["4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20"]  // convert to number range
 
-    @IBOutlet weak var TypeField: UITextField!
-    @IBOutlet weak var LengthField: UITextField!
-    
-    
+
+    @IBOutlet weak var typeField: UIPickerView!
+    @IBOutlet weak var lengthField: UIPickerView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        typeField.delegate = self
+        typeField.dataSource = self
+        
+        lengthField.delegate = self
+        lengthField.dataSource = self
     }
     
+    
+    // Picker code for password type
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        if pickerView == typeField {
+            return passwordTypes.count
+        } else {
+            return lenghtOfPassword.count
+        }
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        if pickerView == typeField {
+            return passwordTypes[row]
+        } else {
+            return lenghtOfPassword[row]
+        }
+    }
+    
+    
+    // Call to generate password
     @IBAction func generateButtonPressed(_ sender: UIButton) {
-        let type = TypeField.text!
-        let length = Int(LengthField.text!)
+//        let type = TypeField.text!
+//        let length = Int(LengthField.text!)
         
-        generatePassword.generate(type: type, length: length)
+//        generatePassword.generate(type: type, length: length)
 
     }
 }
--- PassGen/Model/GeneratePassword.swift
+++ PassGen/Model/GeneratePassword.swift
@@ -12,9 +12,9 @@ struct GeneratePassword {
     
     mutating func generate(type: String!, length: Int!) {
         
-        enum Types {
-            case numeric, lowerCase, upperCase, mixedCase, alphanumeric, alphanumericSpecial
-        }
+//        enum Types {
+//            case numeric, lowerCase, upperCase, mixedCase, alphanumeric, alphanumericSpecial
+//        }
         
         let numeric = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
         let lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
@@ -23,9 +23,12 @@ struct GeneratePassword {
         let alphanumeric = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
         let alphanumericSpecial = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "!", "?", "/", "@", "#", "$", "%", "&"]
         
-        let passwordType = type!
+//        let passwordType = type!
         var passwordList = ""
         
+        func generatePassword(ofType type: String, length long: Int) {
+            
+        }
         
         func getType(for passwordType: String) -> [String] {
             switch passwordType {
