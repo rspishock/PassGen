a5a78ea2902649717e49344acab072f4d3dd0f3d
Ryan Spishock <22054759+rspishock@users.noreply.github.com>
2020-05-08 21:26:32 -0400

    Clean up code and grab values from UIPickerView.  Occasionally receiving skewed results from UIPickerView and Index Out Of Range Error.

--- PassGen/Controller/ViewController.swift
+++ PassGen/Controller/ViewController.swift
@@ -11,12 +11,14 @@ import UIKit
 class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
     
     var generatePassword = GeneratePassword()
-    let passwordTypes = ["Numeric", "Lower Case", "Upper Case", "Mixed Case",  "Alphanumeric", "Alphanumeric With Special Characters"]
-    let lenghtOfPassword = ["4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20"]  // convert to number range
-
 
     @IBOutlet weak var typeField: UIPickerView!
     @IBOutlet weak var lengthField: UIPickerView!
+    
+    var passwordTypes:[String] = [String]()
+    var lengthOfPassword: [String] = [String]()
+    var type = "Numeric"
+    var length = "4"
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,36 +29,51 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
         
         lengthField.delegate = self
         lengthField.dataSource = self
+        
+        // Input data into the arrays
+        passwordTypes = ["Numeric", "Lower Case", "Upper Case", "Mixed Case",  "Alphanumeric", "Alphanumeric With Special Characters"]
+        lengthOfPassword = ["4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20"]
     }
     
     
     // Picker code for password type
+    // The number of columns of data
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
     }
     
+    // The number of rows of data
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
         if pickerView == typeField {
-            return passwordTypes.count
+            return passwordTypes.count  // 6
         } else {
-            return lenghtOfPassword.count
+            return lengthOfPassword.count  // 17
         }
     }
     
+    // The data to return for the row and component (column) that's being passed in
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
         if pickerView == typeField {
             return passwordTypes[row]
         } else {
-            return lenghtOfPassword[row]
+            return lengthOfPassword[row]
         }
     }
     
+    // Capture the picker view selection
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        // This method is triggered whenever the user makes a change to the picker selection
+        // The parameter named row and component represents what was selected
+        type = passwordTypes[row]
+        length = lengthOfPassword[row]
+    }
     
     // Call to generate password
     @IBAction func generateButtonPressed(_ sender: UIButton) {
-//        let type = TypeField.text!
-//        let length = Int(LengthField.text!)
+        print("Type: \(type)")
+        print("Length: \(length)")
         
+
 //        generatePassword.generate(type: type, length: length)
 
     }
