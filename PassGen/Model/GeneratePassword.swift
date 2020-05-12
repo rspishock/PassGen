2c8ff87f3c507b57065bf474a7ea8de4db5d8526
Ryan Spishock <22054759+rspishock@users.noreply.github.com>
2020-05-12 16:45:46 -0400

    Remove password generation functionality out of a function and make it part of mutating generation function.

--- PassGen/Model/GeneratePassword.swift
+++ PassGen/Model/GeneratePassword.swift
@@ -12,59 +12,37 @@ struct GeneratePassword {
     
     mutating func generate(type: String!, length: String!) {
 
+        var passwordList = [String] ()  // empty string to hold password chars
+        var startingLength = 1
         
-//        let numeric = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
-//        let lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-//        let upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-//        let mixedCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-//        let alphanumeric = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-//        let alphanumericSpecial = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "!", "?", "/", "@", "#", "$", "%", "&"]
-//
+        let numeric = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+        let lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+        let upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+        let mixedCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+        let alphanumeric = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+        let alphanumericSpecial = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "!", "?", "/", "@", "#", "$", "%", "&"]
+
 //        let passwordType = type!
+//        let passwordLength = Int(length)!
+        
         
 //        func generatePassword(ofType type: [String], length long: Int) -> String {
-//            var passwordList = [String] ()
-//            var startingLength = 1
-//
-//            repeat {
-//                passwordList.append(type.randomElement()!)
-//                startingLength += 1
-//            } while startingLength <= long
-//
-//            let password = passwordList.joined()
-//            print(password)  // TESTING
+      
+        repeat {
+            passwordList.append(type.randomElement()!)
+            startingLength += 1
+            print(passwordList)
+        } while startingLength <= Int(length)!
+        
+        let password = passwordList.joined()
+        print(password)  // TESTING
 //            return password
-//
-//        }
-//
-//
-//        func getType(for passwordType: String, of length: String) -> [String] {
-//            var password: String
-//            print(passwordType)
-//            switch passwordType {
-//            case "numeric":
-//                return generatePassword(ofType: [numeric], length: length)
-//            case "lowerCase":
-//                return generatePassword(ofType: [lowerCase], length: length)
-//            case "upperCase":
-//                return generatePassword(ofType: [upperCase], length: length)
-//            case "mixedCase":
-//                generatePassword(ofType: [mixedCase], length: length)
-//                return password!
-//            case "alphanumeric":
-//                return generatePassword(ofType: [alphanumeric], length: length)
-//            case "alphanumericSpecial":
-//                return generatePassword(ofType: [alphanumericSpecial], length: length)
-//            default:
-//                break
-//            }
-//            return ["End of func."]  // TESTING
 //        }
         
-        
-        
+//        generatePassword(ofType: [passwordType], length: passwordLength)
+
         // FOR TESTING PURPOSES ONLY!!!!
-        print(type!)
-        print(length!)
+//        print(type!)
+//        print(length!)
     }
 }
